f3736750c24f25fc724c066168dba888fac72ade
Jean-Philippe Bossuat <39432395+Pro7ech@users.noreply.github.com>
2019-01-23 11:09:57 +0100

    Update digibyte.py

--- digibyte.py
+++ digibyte.py
@@ -31,21 +31,22 @@ def compress(vk_hex):
     return prefix + x
 
 
-def decompress(compressed_vk_hex):
-    prefix = compressed_vk_hex[:2]
-    
-    x = int(compressed_vk_hex[2:],16)
+def decompress(compressed_vk):
 
+    prefix = compressed_vk[:1]
+
+    x = int.from_bytes(compressed_vk[1:], byteorder='big')
+    
     p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
     y_squared = (x**3 + 7) % p
     y = pow(y_squared,(p+1)>>2,p)
     
-    if prefix == b'03' and y&1 == 0:
+    if prefix == b'\x03' and y&1 == 0:
         y = p-y
-    elif prefix == b'02' and y&1 == 1:
+    elif prefix == b'\x02' and y&1 == 1:
         y = p-y
-        
-    return '{:032x}{:032x}'.format(x,y)
+ 
+    return compressed_vk[1:] + (y).to_bytes(32,byteorder='big')
     
     
 
@@ -68,52 +69,54 @@ def generateAddress(prefix=b''):
 
     
 def sign_digest(digest,sk):
-    return hexlify(sk.sign_digest(digest,sigencode=ecdsa.util.sigencode_der))+ b'01'
+    return hexlify(sk.sign_digest(digest,sigencode=ecdsa.util.sigencode_der))+ b'\x01'
 
 def verify_digest(digest,sig,vk):
-    return vk.verify_digest(unhexlify(sig)[:-1],digest,sigdecode=ecdsa.util.sigdecode_der)
+    return vk.verify_digest(sig[:-1],digest,sigdecode=ecdsa.util.sigdecode_der)
 
-#TODO : direcly parse bytes
-def parseVariableLengthInteger(data):
 
-    size = int.from_bytes(data[:1], byteorder='big')
+def parseVariableInteger(data):
+
+    size = int.from_bytes(data[:1], byteorder='little')
     
     if size<0xFD:
         return data[:1]
     elif size == 0xFD:
-        return data[:6]
+        return data[:3]
     elif size == 0xFE:
-        return data[:10]
+        return data[:5]
     elif size == 0xFF:
-        return data[:18]
+        return data[:9]
     else:
         raise ValueError('Invalid variable length integer input')
 
-def getVariableLengthInteger(data):
-    size = int.from_bytes(data[:1], byteorder='big')
+
+def getVariableIntegerValue(data):
+    size = int.from_bytes(data[:1], byteorder='little')
     
     if size<0xFD:
         return size
     elif size == 0xFD:
-        return int.from_bytes(data[2:6], byteorder='little') 
+        return int.from_bytes(data[1:3], byteorder='little') 
     elif size == 0xFE:
-        return int.from_bytes(data[2:10], byteorder='little')
+        return int.from_bytes(data[1:5], byteorder='little')
     elif size == 0xFF:
-        return int.from_bytes(data[2:18], byteorder='little')
+        return int.from_bytes(data[1:9], byteorder='little')
     else:
         raise ValueError('Invalid variable length integer input')
     
     
 #TODO : direcly parse bytes
-def encodeVariableLengthInteger(integer):
+def encodeVariableInteger(integer):
+    
     if 0 <= integer < 0xFD:
-        return bytes('{:02x}'.format(integer),'utf-8')
+        return (integer).to_bytes(1,byteorder='little')
     elif 0xFC < integer < 0x10000:
-        return bytes('FD{:04x}'.format(integer),'utf-8')
+        return b'\xfd' + (integer).to_bytes(2,byteorder='little')
     elif 0xFFFF < integer < 0x100000000:
-        return bytes('FE{:08x}'.format(integer),'utf-8')
+        return b'\xFE' + (integer).to_bytes(4,byteorder='little')
     elif 0xFFFFFFFF < integer < 0x10000000000000000:
-        return bytes('FF{:016x}'.format(integer),'utf-8')
+        return b'\xff' + (integer).to_bytes(8,byteorder='little')
     else:
         raise ValueError('Invalid integer')
 
@@ -122,20 +125,17 @@ def encodeVariableLengthInteger(integer):
 def parseSignedRawTransaction(transaction):
     transaction = unhexlify(transaction)
     
-    
     TX = {}
     pointer = 0
     TX['format'] = transaction[pointer:pointer+4]
     pointer += 4
     
-    TX['Inputs'] = parseVariableLengthInteger(transaction[pointer:pointer+9])
+    TX['Inputs'] = parseVariableInteger(transaction[pointer:pointer+9])
     pointer += len(TX['Inputs'])
-
-    print(hexlify(TX['Inputs']))
     
     TX['vin'] = []
     
-    for i in range(getVariableLengthInteger(TX['Inputs'])):
+    for i in range(getVariableIntegerValue(TX['Inputs'])):
         tmp = {}
         
         tmp['txid'] = transaction[pointer:pointer+32]
@@ -144,41 +144,35 @@ def parseSignedRawTransaction(transaction):
         tmp['Index'] = transaction[pointer:pointer+4]
         pointer += 4
         
-        tmp['Length'] = parseVariableLengthInteger(transaction[pointer:pointer+9])
+        tmp['Length'] = parseVariableInteger(transaction[pointer:pointer+9])
         pointer += len(tmp['Length'])
         
-        scriptLength = getVariableLengthInteger(tmp['Length'])
+        scriptLength = getVariableIntegerValue(tmp['Length'])
         
         tmp['ScriptSig'] = transaction[pointer:pointer+scriptLength]
-        
         pointer += scriptLength
         
         tmp['Sequence'] = transaction[pointer:pointer+4]
-        
         pointer += 4
 
         TX['vin'] += [tmp]
-
         
-    TX['Outputs'] = parseVariableLengthInteger(transaction[pointer:pointer+9])
-    
+    TX['Outputs'] = parseVariableInteger(transaction[pointer:pointer+9])
     pointer += len(TX['Outputs'])
 
     TX['vout'] = []
 
-    for i in range(getVariableLengthInteger(TX['Outputs'])):
+    for i in range(getVariableIntegerValue(TX['Outputs'])):
         tmp = {}
         tmp ['Value'] = transaction[pointer:pointer+8]
         pointer += 8
         
-        tmp ['Length'] = parseVariableLengthInteger(transaction[pointer:pointer+9])
-        
+        tmp ['Length'] = parseVariableInteger(transaction[pointer:pointer+9])
         pointer += len(tmp ['Length'])
         
-        scriptLength = getVariableLengthInteger(tmp['Length'])
+        scriptLength = getVariableIntegerValue(tmp['Length'])
         
         tmp['ScriptPubKey'] = transaction[pointer:pointer+scriptLength]
-        
         pointer += scriptLength
         
         TX['vout'] += [tmp]
@@ -187,7 +181,8 @@ def parseSignedRawTransaction(transaction):
 
     return TX
 
-#TODO : direcly parse bytes
+
+
 def verifySignedRawTransaction(transaction,inputs):
     TX = parseSignedRawTransaction(transaction)
 
@@ -197,23 +192,33 @@ def verifySignedRawTransaction(transaction,inputs):
         
         scriptSig = cleanTX['vin'][i]['ScriptSig']
 
-        scriptSigLen = parseVariableLengthInteger(scriptSig)
+        PUSHDATA = int.from_bytes(scriptSig[:1],byteorder='big')
+        scriptSig = scriptSig[1:]
         
-        signature = scriptSig[len(scriptSigLen):(int(scriptSigLen,16)<<1)+len(scriptSigLen)]
+        signature = scriptSig[:PUSHDATA]
+        scriptSig = scriptSig[PUSHDATA:]
 
-        vk = VerifyingKey.from_string(unhexlify(decompress(scriptSig[len(scriptSigLen)+(int(scriptSigLen,16)<<1)+2:])), curve=SECP256k1)
+        PUSHDATA = int.from_bytes(scriptSig[:1],byteorder='big')
+        scriptSig = scriptSig[1:]
+        
+        compressed_vk = scriptSig[:PUSHDATA]
+        scriptSig = scriptSig[PUSHDATA:]
 
-        if sha256ripemd160(unhexlify(compress(hexlify(vk.to_string())))) != unhexlify(bytes(inputs[i]['scriptPubKey'],'utf-8'))[3:-2]:
+        vk = VerifyingKey.from_string(decompress(compressed_vk), curve=SECP256k1)
+
+        pubKeyHash = inputs[i]['scriptPubKey'][3:-2]
+
+        if sha256ripemd160(compressed_vk) != pubKeyHash:
             return False
-
+        
         for j in range(len(TX['vin'])):
-            cleanTX['vin'][j]['Length'] = b'00'
+            cleanTX['vin'][j]['Length'] = b'\x00'
             cleanTX['vin'][j]['ScriptSig'] = b''
         
         tmp = cleanTX.copy()
         
-        tmp['vin'][i]['Length'] = encodeVariableLengthInteger(len(inputs[i]['scriptPubKey'])>>1)
-        tmp['vin'][i]['ScriptSig'] = bytes(inputs[i]['scriptPubKey'],'utf-8')
+        tmp['vin'][i]['Length'] = encodeVariableInteger(len(inputs[i]['scriptPubKey']))
+        tmp['vin'][i]['ScriptSig'] = inputs[i]['scriptPubKey']
 
         rawtmp = b''
         rawtmp += tmp['format']
@@ -234,9 +239,9 @@ def verifySignedRawTransaction(transaction,inputs):
             rawtmp += tmp['vout'][j]['ScriptPubKey']
 
         rawtmp += tmp['Locktime']
-        rawtmp += signature[-2:] + b'000000'
+        rawtmp += signature[-1:] + b'\x00\x00\x00'
 
-        signed = dsha256(unhexlify(rawtmp))
+        signed = dsha256(rawtmp)
   
         v = verify_digest(signed,signature,vk)
         if v != True:
@@ -244,8 +249,8 @@ def verifySignedRawTransaction(transaction,inputs):
     return True
         
 
-inputs = [{"scriptPubKey":"76a914128bef368aee81f7f89a0206e77aabbfd5b4f05b88ac"},
-          {"scriptPubKey":"76a914128bef368aee81f7f89a0206e77aabbfd5b4f05b88ac"}]
+inputs = [{"scriptPubKey":unhexlify("76a914128bef368aee81f7f89a0206e77aabbfd5b4f05b88ac")},
+          {"scriptPubKey":unhexlify("76a914128bef368aee81f7f89a0206e77aabbfd5b4f05b88ac")}]
 
 x = b'010000000272f5fe89b3db1343301384ed5981b4b3e1cfabe9abcb48b92cdfae1c34b55ac7020000006a473044022041be4bf959fb9296872b71b17580b4ec0be79ccc2f7232360b1af23148b0797f02205bca0687905e219fe4b068c7a8fd6f231827a44fee10145af6df7fa5d1f7e319012103a81a7d62c25af7945e225b74f75478e53672b585d9ac8ec88344f4b92f34e843ffffffff72f5fe89b3db1343301384ed5981b4b3e1cfabe9abcb48b92cdfae1c34b55ac7000000006a47304402201bfe24973497bf7b15dba2ecbae03ed496c60f03e8998f1aaf8971367f63637b022042e5ccb818dbc931e77a8dc935bef004f30263e11176a791a35f0db0f1b80536012103a81a7d62c25af7945e225b74f75478e53672b585d9ac8ec88344f4b92f34e843ffffffff03c0270900000000001976a914128bef368aee81f7f89a0206e77aabbfd5b4f05b88ac0000000000000000216a1f54686973206973206120746573742031353a31312032352e31312e3230313820961877000000001976a914128bef368aee81f7f89a0206e77aabbfd5b4f05b88ac00000000'
 v = verifySignedRawTransaction(x,inputs)
